2b327b9152e71fdf1e894ae1a8b9fd8bc68ed93b
wohajo <m.prawda99@gmail.com>
2020-07-23 18:13:19 +0200

    basic stats scrape

--- database_update.py
+++ database_update.py
@@ -4,19 +4,6 @@ import requests
 
 #TODO make dict with every type of vehicle and write it to a file
 
-def get_vehicles_links():
-
-    url = scrape_links[0]
-
-    # raw HTML
-    html_content = requests.get(url).text
-
-    soup = BeautifulSoup(html_content, "lxml")
-
-    for link in soup.findAll("div", {"class": "mw-category-group"}):
-        for li in link.findAll("li"):
-            print(li.a.get("href"))
-
 scrape_links = [
     'https://wiki.warthunder.com/Category:First_rank_aircraft', 
     'https://wiki.warthunder.com/Category:Second_rank_aircraft', 
@@ -41,5 +28,27 @@ scrape_links = [
     'https://wiki.warthunder.com/Category:Seventh_rank_helicopters'
     ]
 
+def get_vehicles_links():
+
+    url = scrape_links[0]
+
+    html_content = requests.get(url).text
+
+    soup = BeautifulSoup(html_content, "lxml")
+
+    for link in soup.findAll("div", {"class": "mw-category-group"}):
+        for li in link.findAll("li"):
+            print(li.a.get("href"))
+
+def get_basic_vehicle_stats(url):
+
+    html_content = requests.get(url).text
+
+    soup = BeautifulSoup(html_content, "lxml")
+
+    for link in soup.find_all("span", {"class": "ttx-value"}):
+        print(link.text)
+
 if __name__ == "__main__":
-    get_vehicles_links()
+    # get_vehicles_links()
+    get_basic_vehicle_stats('https://wiki.warthunder.com/He_51_B-1')
