f889e5320042cb94c613e4fe2788e5428081ace3
wohajo <michalos222@gmail.com>
2020-07-27 13:09:01 +0200

    added JS for opening menu on mobile

---
+++ static/js/scripts.js
@@ -0,0 +1,16 @@
+var menuToggle = document.getElementById("menu-sidebar-toggle")
+var menu = document.getElementById("menu-sidebar")
+
+menuToggle.addEventListener("click", menuToggleClick)
+
+function menuToggleClick() {
+    menu.classList.toggle("sidebar--open")
+    // TODO change hamburger icon to X
+    // if (menu.style.width = 0) {
+    //     menu.classList.add("sidebar--open")
+    //     menuToggle.innerHTML = ("&times")
+    // } else {
+    //     menu.classList.remove("sidebar--open")
+    //     menuToggle.innerHTML = ("&#9776")
+    // }
+}
