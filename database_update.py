6f9b987d3fd8ba922ac312b3d472831a873e7893
wohajo <michalos222@gmail.com>
2020-07-30 12:10:04 +0200

    added basic database classes

--- database_update.py
+++ database_update.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
+from flask_sqlalchemy import SQLAlchemy
 import requests
+
 
 scrape_links = [
     'https://wiki.warthunder.com/Category:First_rank_aircraft', 
@@ -38,18 +40,27 @@ def get_vehicles_links():
             print(li.a.get("href"))
 
 def get_basic_vehicle_stats(url):
-
+    '''
+    Returns basic vehicle statistics included in table
+    on the right of the webpage in form of a raw list.  
+    '''
     html_content = requests.get(url).text
 
     soup = BeautifulSoup(html_content, "lxml")
-    tags = soup.select('div[class="ttx-title"], span[class="ttx-value"], span[class="ttx-rb ttx-value"], span[class="ttx-value ttx-rb"], div[class="ttx-table-line ttx-table-head"]')
+    tags = soup.select('span[class="ttx-country"], span[class="ttx-rank"], div[class="ttx-title"], span[class="ttx-value"], span[class="ttx-rb ttx-value"], span[class="ttx-value ttx-rb"], div[class="ttx-table-line ttx-table-head"]')
 
     for tag in tags:
         items = [item.text for item in tags if item.text.strip() != '']
 
-    print('\n'.join(items))
+    # print('\n'.join(items))
+    print(items)
+
+    return items
 
 def get_planes_tables(url):
+    '''
+    [IN PROGRESS] Prints all tables from the webpage.
+    '''
 
     html_content = requests.get(url).text
 
@@ -63,7 +74,10 @@ def get_planes_tables(url):
 
     print(tables[1].text)
 
+def basic_stats_to_list(items):
+    '''
+    [IN PROGRESS] Returns basic plane info in better form (object?).
+    '''
+
 if __name__ == "__main__":
-    # get_basic_vehicle_stats('https://wiki.warthunder.com/J35D')
-    get_modules_info('https://wiki.warthunder.com/J35D')
-    get_modules_info('https://wiki.warthunder.com/Ju_87_B-2')
+    get_basic_vehicle_stats('https://wiki.warthunder.com/J35D')
