7facb1154b760c155949742e45b37501f2e236a8
wohajo <michalos222@gmail.com>
2020-08-20 13:48:34 +0200

    fixed economy reading for vehicles with free repair

--- helpers_planes.py
+++ helpers_planes.py
@@ -82,23 +82,24 @@ def process_economy(lst, url):
         lst = []
         return lst
     else:
-        return None
-        
-    new_list = [word.replace(' %', '').replace('free', '0') for word in lst]
-    #TODO: FIX if there is a free repair 
-    # there is no max repair sb/rb/ab and no crew training
-    
-    repair_sb = new_list[2].partition('/')
-    repair_rb = new_list[3].partition('/')
-    repair_ab = new_list[4].partition('/')
-    new_list[2] = repair_sb[0] 
-    new_list[3] = repair_rb[0]
-    new_list[4] = repair_ab[0]
-    new_list.insert(3, repair_sb[2])
-    new_list.insert(5, repair_rb[2])
-    new_list.insert(7, repair_ab[2])
-    new_list = [int(word.replace(' ', '')) for word in new_list]
-    return new_list
+        new_list = [word.replace(' %', '').replace(' ', '') for word in lst]
+        if 'free' not in lst:
+            repair_sb = new_list[2].partition('/')
+            repair_rb = new_list[3].partition('/')
+            repair_ab = new_list[4].partition('/')
+            new_list[2] = repair_sb[0] 
+            new_list[3] = repair_rb[0]
+            new_list[4] = repair_ab[0]
+            new_list.insert(3, repair_sb[2])
+            new_list.insert(5, repair_rb[2])
+            new_list.insert(7, repair_ab[2])
+        else:
+            new_list = [word.replace('free', '0') for word in new_list]
+            for i in range(3, 9):
+                new_list.insert(i, '0')
+
+        new_list = [int(word) for word in new_list]
+        return new_list
 
 def process_characteristics_table(lst, url):
     if any(word in WORD_TO_REMOVE_PROCESSING for word in lst) or len(lst[0]) != 8 or len(lst[1]) not in [6, 8]:
